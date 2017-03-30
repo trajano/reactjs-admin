afb079cb4c7292441e28c2d571ede5bfa63f7843
Archimedes Trajano <arch@trajano.net>
2017-03-30 16:19:50 -0400

    Added the main wrapped areas

--- src/app.js
+++ src/app.js
@@ -3,7 +3,13 @@ import ReactDOM from 'react-dom'
 import './app.scss'
 
 const AppComponent = props => (
-    <div>hello world</div>
+    <div id="wrapper">
+        <nav className="navbar navbar-default navbar-static-top" role="navigation" style={{marginBottom: 0}}>
+        </nav>
+        <div id="page-wrapper">
+            hello world
+        </div>
+    </div>
 )
 
 ReactDOM.render(<AppComponent />, document.getElementById("app"))
