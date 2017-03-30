c9027c7bb2245172f195bdde588bc8c83eab1d24
Archimedes Trajano <arch@trajano.net>
2017-03-30 16:26:58 -0400

    Moved navigation into its own file

---
+++ src/Navigation.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+export const Navigation = props => (
+    <nav className="navbar navbar-default navbar-static-top" role="navigation" style={{ marginBottom: 0 }}>
+    </nav>
+)
--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './app.scss'
+import { Navigation } from './Navigation.js'
 
 const AppComponent = props => (
     <div id="wrapper">
-        <nav className="navbar navbar-default navbar-static-top" role="navigation" style={{marginBottom: 0}}>
-        </nav>
+        <Navigation />
         <div id="page-wrapper">
             hello world
         </div>
