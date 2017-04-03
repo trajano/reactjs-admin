326d9e100e01540449ceeb8feb822a74668094f0
Archimedes Trajano <arch@trajano.net>
2017-04-03 16:25:11 -0400

    fixup! Removed redux and refactored SideMenu

--- src/components/redux/actions.js
+++
@@ -1,9 +0,0 @@
-export const UPDATE_NAV_ACTIVE_PATH = Symbol("UPDATE_NAV_ACTIVE_PATH")
-
-/**
- * Sets a mew navigation path
- * @param {number[]} newActivePath new active path
- */
-export function updateNavActivePath(newActivePath) {
-    return { type: UPDATE_NAV_ACTIVE_PATH, newActivePath }
-}
