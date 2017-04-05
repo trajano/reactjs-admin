06dee49913ff5b16a163ddde3a64e362ef09b17a
Archimedes Trajano <developer@trajano.net>
2017-04-05 01:42:25 -0400

    Removed test code

--- src/app.js
+++ src/app.js
@@ -1,11 +1,3 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import {
-    BrowserRouter as Router,
-    Route,
-    Link
-} from 'react-router-dom'
-import Icon from './components/Icon'
 import { bootstrap } from './components/Module'
 
 import MyBlank from './myapp/Blank'
@@ -15,73 +7,6 @@ import SamplePages from './myapp/SamplePages'
 import SecondPage from './myapp/SecondPage'
 import ThirdLevelItem from './myapp/ThirdLevelItem'
 import FourOhFour from './myapp/FourOhFour'
-
-const Home = () => (
-    <div>
-        <h2>Home</h2>
-    </div>
-)
-
-const About = () => (
-    <div>
-        <h2>About</h2>
-    </div>
-)
-
-const Topics = ({ match }) => (
-    <div>
-        <h2>Topics</h2>
-        <ul>
-            <li>
-                <Link to={`${match.url}/rendering`}>
-                    Rendering with React
-        </Link>
-            </li>
-            <li>
-                <Link to={`${match.url}/components`}>
-                    Components
-        </Link>
-            </li>
-            <li>
-                <Link to={`${match.url}/props-v-state`}>
-                    Props v. State
-        </Link>
-            </li>
-        </ul>
-
-        <Route path={`${match.url}/:topicId`} component={Topic} />
-        <Route exact path={match.url} render={() => (
-            <h3>Please select a topic.</h3>
-        )} />
-    </div>
-)
-
-const Topic = ({ match }) => (
-    <div>
-        <h3>{match.params.topicId}</h3>
-    </div>
-)
-
-const BasicExample = () => (
-    <Router>
-        <div>
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/about">About</Link></li>
-                <li><Link to="/topics">Topics</Link></li>
-            </ul>
-
-            <hr />
-
-            <Route exact path="/" component={Home} />
-            <Route path="/about" component={About} />
-            <Route path="/topics" component={Topics} />
-
-            <hr />
-        </div>
-    </Router>
-)
-
 
 /** @type {ModuleConfig} */
 const moduleConfig = {
@@ -137,7 +62,7 @@ const moduleConfig = {
                 {
                     label: 'Second Level Item',
                     to: '/s2',
-                    component: ThirdLevelItem
+                    component: Forms
                 },
                 {
                     label: 'Third Level',
