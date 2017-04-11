88b6c0498e6dcf159ba4dc184ef40bfcdbeb7abf
Archimedes Trajano <developer@trajano.net>
2017-04-10 23:57:51 -0400

    X

--- src/framework/SideNav.jsx
+++ src/framework/SideNav.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Icon from './Icon'
 export default class SideNav extends React.Component {
     static propTypes = {
         /**
@@ -10,6 +11,38 @@ export default class SideNav extends React.Component {
          */
         visible: React.PropTypes.bool.isRequired
     }
-    render() { return (<div className="sidebar col-sm-3 col-md-2 hidden-xs-down bg-faded" >asdf
-        </div>) }
+    render() {
+        return (<nav className="sidebar col-sm-3 col-md-2 hidden-xs-down bg-faded" role="navigaton">
+            <ul className="nav">
+                <li className=""><a href="#" ><Icon name="dashboard" fw /> Dashboard</a></li>
+                <li className=""><a href="#" ><Icon name="eye" fw /> Eye</a></li>
+                <li className=""><a href="#" ><Icon name="dashboard" fw /> First Level</a>
+                    <ul className="nav ">
+                        <li className=""><a href="#">Second Level</a></li>
+                        <li className=""><a href="#">Second Level</a></li>
+                    </ul>
+                </li>
+                <li className=""><a href="#">asdf</a></li>
+                <li className=""><a href="#">asdf</a></li>
+                <li className=""><a href="#">asdf</a>
+                    <ul className="nav ">
+                        <li className=""><a href="#">asdf</a></li>
+                        <li className=""><a href="#">Second Level</a>
+                            <ul className="nav">
+                                <li className=""><a href="#">Third Level</a></li>
+                                <li className=""><a href="#">Third Level</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+                <li className="nav-item"><a href="#">asdf</a>
+                    <ul className="nav nav-pills">
+                        <li className="nav-item"><a href="#">asdf</a></li>
+                        <li className="nav-item"><a href="#">asdf</a></li>
+                    </ul>
+                </li>
+                <li className="nav-item"><a href="#">asdf</a></li>
+            </ul>
+        </nav>)
+    }
 }
