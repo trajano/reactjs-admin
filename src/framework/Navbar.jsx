86d86cd5b43acf56829aae4d853b87848283f7a6
Archimedes Trajano <developer@trajano.net>
2017-04-17 20:44:31 -0400

    Removed unused code

--- src/framework/Navbar.jsx
+++ src/framework/Navbar.jsx
@@ -149,38 +149,4 @@ export default class Navbar extends React.Component {
             </ul>
         </nav>)
     }
-    render2() {
-        if (this.props.smallDeviceNavigation) {
-            return (<nav className="navbar navbar-light bg-faded fixed-top" role="navigation">
-                <div className="navbar-header">
-                    <button type="button" className="navbar-toggler navbar-toggler-left" aria-label="Toggle navigation" onClick={this.props.toggleSideNav}>
-                        <span className="navbar-toggler-icon"></span></button>
-                    <div className="pull-right">
-                        <span className="pull-right"><a className="nav-link" href="#"><Icon name="user" fw /></a>
-                        </span>
-                        <span className="pull-right"><a className="nav-link" href="#"><Icon name="comment" fw /></a>
-                        </span>
-                        <span className="pull-right"><a className="nav-link" href="#"><Icon name="tasks" fw /></a>
-                        </span>
-                        <span className="pull-right"><a className="nav-link" href="#"><Icon name="envelope" fw /></a></span>
-                    </div>
-                </div>
-            </nav>)
-        } else {
-            return (<nav className="navbar navbar-light bg-faded fixed-top" role="navigation">
-                <div className="navbar-header">
-                    <Link className="navbar-brand mr-auto" to="/"><img src={this.props.logo} srcSet={this.props.logo.srcSet} alt="" />{this.props.title}</Link>
-                    <div className="pull-right">
-                        <span className="pull-right"><a className="nav-link" href="#"><Icon name="user" fw /></a>
-                        </span>
-                        <span className="pull-right"><a className="nav-link" href="#"><Icon name="comment" fw /></a>
-                        </span>
-                        <span className="pull-right"><a className="nav-link" href="#"><Icon name="tasks" fw /></a>
-                        </span>
-                        <span className="pull-right"><a className="nav-link" href="#"><Icon name="envelope" fw /></a></span>
-                    </div>
-                </div>
-            </nav>)
-        }
-    }
 }
